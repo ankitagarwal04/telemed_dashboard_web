f77025a3048ded79e7b45c98531ca5a978fbd55b
ankit agarwal <ankit.mrka4@gmail.com>
2021-02-08 12:04:04 +0530

    fixes navbar

--- src/views/web/components/core/Navbar.vue
+++ src/views/web/components/core/Navbar.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- eslint-disable -->
   <b-navbar
     toggleable="lg"
     type="light"
@@ -6,27 +7,32 @@
   >
     <b-navbar-brand href="#">
       <img src="~@/assets/images/csc_logo.png">
-      <span>Telemedicine</span>
     </b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse" />
 
     <b-collapse
       id="nav-collapse"
+      class="justify-content-center"
       is-nav
     >
-      <b-navbar-nav class="ml-auto">
-        <b-nav-item href="#">
+      <b-navbar-nav>
+        <b-nav-item href="#" class="pr-3">
           <span>VLE</span>
         </b-nav-item>
-        <b-nav-item href="#">
-          Doctor
+        <b-nav-item href="#" class="pr-3">
+          <span>Doctor</span>
         </b-nav-item>
-        <b-nav-item href="/dashboard">
-          Dashboard
+        <b-nav-item href="/dashboard" class="pr-3">
+          <span>Dashboard</span>
         </b-nav-item>
       </b-navbar-nav>
     </b-collapse>
+
+    <b-navbar-brand href="#">
+      <img src="~@/assets/images/digital_india_logo.png">
+    </b-navbar-brand>
+
   </b-navbar>
 </template>
 
