636b17e25f5381e74e456b2c5b7221a33fb9fe01
ankit agarwal <ankit.mrka4@gmail.com>
2021-03-03 14:03:28 +0530

    small change

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
+// =========================================================
+// * Vuetify Material Dashboard - v2.1.0
+// =========================================================
+//
+// * Product Page: https://www.creative-tim.com/product/vuetify-material-dashboard
+// * Copyright 2019 Creative Tim (https://www.creative-tim.com)
+//
+// * Coded by Creative Tim
+//
+// =========================================================
+//
+// * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm'
 import App from './App.vue'
