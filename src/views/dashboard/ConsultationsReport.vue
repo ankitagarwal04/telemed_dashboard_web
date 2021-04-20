b70ac3c3c74e64aa42af683d95b25a72c0180091
ankit agarwal <ankit.mrka4@gmail.com>
2021-04-20 11:04:01 +0530

    in progress

--- src/views/dashboard/ConsultationsReport.vue
+++ src/views/dashboard/ConsultationsReport.vue
@@ -29,7 +29,7 @@
       striped
       hover
       :items="consultationsData.consultations"
-      :fields="fields"
+      :fields="consultationsData.fields"
       small
     >
       <template #cell(index)="data">
@@ -43,34 +43,65 @@
         :total-rows="consultationsData.consultationsCount"
         :per-page="perPage"
       />
-      <download-excel
+      <!-- <download-excel
         class="btn btn-info h-100"
         :data="consultationsData.downloadConsultations"
         type="csv"
         name="doctors_successful_consultations.xls"
       >
         Download CSV
-      </download-excel>
+      </download-excel> -->
+      <b-button
+        class="btn btn-info h-100"
+        @click="startReportDownload()"
+      >
+        Start Download
+      </b-button>
     </div>
+    <!-- downloadble reports section-->
+    <b-button
+      class="btn btn-info h-100"
+      @click="downloadImage()"
+    >
+      File Download
+    </b-button>
+    <hr>
+    <b-table
+      striped
+      hover
+      :items="downloadedReports.reports"
+      :fields="downloadedReports.fields"
+      small
+    >
+      <template #cell(index)="data">
+        {{ (currentPage - 1)*perPage + data.index + 1 }}
+      </template>
+      <template #cell(download_report)="data">
+        {{ (currentPage - 1)*perPage + data.index + 1 }}
+      </template>
+    </b-table>
   </div>
 </template>
 
 <script>
   import format from 'date-fns/format'
-  // TODO: download report needs to be implemented through background job.
+  import axios from 'axios'
   export default {
     name: 'ConsultationsReport',
     components: {
-      DownloadExcel: () => import('vue-json-excel'),
+      // DownloadExcel: () => import('vue-json-excel'),
       StatsCard: () => import('@/components/StatsCard'),
     },
     data () {
       return {
-        fields: ['index', 'doctorFullName', 'doctorEmail', 'phoneNumber', 'speciality'],
         consultationsData: {
           consultations: [],
-          downloadConsultations: [],
           consultationsCount: 0,
+          fields: ['index', 'doctorFullName', 'doctorEmail', 'phoneNumber', 'speciality'],
+        },
+        downloadedReports: {
+          reports: [],
+          fields: ['index', 'name', 'status', 'started_at', 'finished_at'],
         },
         perPage: 10,
         currentPage: 1,
@@ -90,12 +121,43 @@
     },
     created () {
       this.getConsultations('')
+      this.getDownloadedConsultationReports()
     },
     methods: {
+      // sample method to download image, retrived from the api server.
+      // this method can be used as a reference (for generating a url) to download a file.
+      downloadImage: function () {
+        axios({
+          url: 'https://source.unsplash.com/random/500*500',
+          method: 'GET',
+          responseType: 'blob',
+        }).then((response) => {
+          const url = window.URL.createObjectURL(new Blob([response.data]))
+          const link = document.createElement('a')
+          link.href = url
+          // link.setAttribute('download', 'file.pdf')
+          link.setAttribute('download', 'image.jpg')
+          document.body.appendChild(link)
+          link.click()
+        })
+      },
       resetConsultations: function () {
         this.consultationsData.consultations = []
-        this.consultationsData.downloadConsultations = []
         this.consultationsData.consultationsCount = 0
+      },
+      startReportDownload: function () {
+        this.$http.post('/dashboard_consultations/download_report', {
+          consultation_filters: {
+            date_from: this.datePicker.dateOne,
+            date_to: this.datePicker.dateTwo,
+          },
+        }).then((response) => {
+          this.$alertify.success('Report Downloading is in process.')
+          this.getDownloadedConsultationReports()
+        }).catch((error) => {
+          // handle error
+          console.log(error)
+        })
       },
       getConsultations: function (instruction) {
         let consultationUrl = '/dashboard_consultations/index'
@@ -106,14 +168,12 @@
           addedString = `?page_num=${this.currentPage}&per_page=${this.perPage}&only_count=true`
         }
         consultationUrl += addedString
-        console.log(consultationUrl)
         this.$http.post(consultationUrl, {
           consultation_filters: {
             date_from: this.datePicker.dateOne,
             date_to: this.datePicker.dateTwo,
           },
         }).then((response) => {
-          console.log(response)
           if (response.all_consultations) {
             this.consultationsData.downloadConsultations = []
             response.all_consultations.forEach((consultation) => {
@@ -137,10 +197,16 @@
             })
           }
           if (response.total_count >= 0) {
-            console.log(response.total_count)
-            console.log(response)
             this.consultationsData.consultationsCount = response.total_count
           }
+        }).catch((error) => {
+          // handle error
+          console.log(error)
+        })
+      },
+      getDownloadedConsultationReports: function () {
+        this.$http.get('/downloadable_consultation_report/index').then((response) => {
+          this.downloadedReports.reports = response
         }).catch((error) => {
           // handle error
           console.log(error)
